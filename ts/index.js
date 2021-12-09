22d3f41f4f1207724b88936e037cf06bcda54e14
zterhes <zterhes1@gmail.com>
2021-12-09 01:10:29 +0100

    delete js files

--- ts/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const day1_1 = __importDefault(require("./solutions/day1"));
-console.log(`day1`, day1_1.default);
